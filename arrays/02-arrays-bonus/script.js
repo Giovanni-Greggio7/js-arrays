e9a4ad3943daa53f570d62d5b34e742077bef278
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-01-13 13:47:33 +0100

    Add: Bonus complete

--- arrays/02-arrays-bonus/script.js
+++ arrays/02-arrays-bonus/script.js
@@ -24,12 +24,16 @@ for ( i = 0; i < teachers.length; i++){
 console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(5, 1)
+let indexEd = teachers.indexOf ("Ed")
+teachers.splice(indexEd, 1)
 console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = ;
+const isFabioPresent = teachers.indexOf ("Fabio") !== -1
+console.log(isFabioPresent)
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+console.log(teachersString)
